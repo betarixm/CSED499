410b4a80217dc7c08ed8dae2e4279163c15c2cca
beta <beta.underalpha@gmail.com>
2022-03-24 02:00:48 +0900

    (#4)(#2) Refactor: Mnist classifier using abstraction

--- src/victim/models.py
+++ src/victim/models.py
@@ -1,86 +1,53 @@
+from typings.models import Model
+from utils.dataset import Mnist as MnistDataset
+
 import tensorflow as tf
 
 keras = tf.keras
 
 
-class Mnist:
-    model = keras.Sequential(
-        [
-            keras.layers.Conv2D(32, (3, 3), activation="relu", input_shape=(28, 28, 1)),
-            keras.layers.MaxPooling2D((2, 2)),
-            keras.layers.Conv2D(64, (3, 3), activation="relu"),
-            keras.layers.MaxPooling2D((2, 2)),
-            keras.layers.Conv2D(64, (3, 3), activation="relu"),
-            keras.layers.Flatten(),
-            keras.layers.Dense(64, activation="relu"),
-            keras.layers.Dense(10, activation="softmax"),
-        ],
-        name="victim_mnist",
-    )
-
-    model.summary()
-
-    checkpoint_filepath = "./checkpoint/mnist"
-    checkpoint_callback = keras.callbacks.ModelCheckpoint(
-        filepath=checkpoint_filepath,
-        save_weights_only=True,
-        monitor="val_accuracy",
-        mode="max",
-        save_best_only=True,
-    )
-
+class Mnist(Model):
     def __init__(
         self,
-        optimizer=keras.optimizers.Adam(),
-        loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-        accuracy=keras.metrics.SparseCategoricalAccuracy(name="accuracy"),
+        data_train: tf.data.Dataset,
+        data_test: tf.data.Dataset,
+        input_shape: tuple,
+        optimizer: keras.optimizers.Optimizer = keras.optimizers.Adam(),
+        loss: keras.losses.Loss = keras.losses.SparseCategoricalCrossentropy(),
+        accuracy: keras.metrics.Accuracy = keras.metrics.SparseCategoricalAccuracy(
+            name="accuracy"
+        ),
+        checkpoint_filepath: str = None,
     ):
-        self.optimizer = optimizer
-        self.loss = loss
-        self.accuracy = accuracy
-
-    def dataset(self):
-        mnist = keras.datasets.mnist
-
-        (x_train, y_train), (x_test, y_test) = mnist.load_data()
-        x_train, x_test = x_train / 255.0, x_test / 255.0
-
-        x_train = x_train[..., tf.newaxis].astype("float32")
-        x_test = x_test[..., tf.newaxis].astype("float32")
-
-        train_ds = (
-            tf.data.Dataset.from_tensor_slices((x_train, y_train))
-            .shuffle(10000)
-            .batch(32)
+        super().__init__(
+            data_train,
+            data_test,
+            input_shape,
+            optimizer,
+            loss,
+            accuracy,
+            checkpoint_filepath,
         )
 
-        test_ds = tf.data.Dataset.from_tensor_slices((x_test, y_test)).batch(32)
+    def name(self) -> str:
+        return "victim_mnist"
 
-        return train_ds, test_ds
-
-    def train(self, epochs: int = 100):
-        train_ds, test_ds = self.dataset()
-
-        self.model.compile(
-            optimizer=self.optimizer,
-            loss=self.loss,
-            metrics=[self.accuracy],
+    def model(self) -> keras.Model:
+        return keras.Sequential(
+            [
+                keras.layers.Conv2D(32, (3, 3), activation="relu"),
+                keras.layers.MaxPooling2D((2, 2)),
+                keras.layers.Conv2D(64, (3, 3), activation="relu"),
+                keras.layers.MaxPooling2D((2, 2)),
+                keras.layers.Conv2D(64, (3, 3), activation="relu"),
+                keras.layers.Flatten(),
+                keras.layers.Dense(64, activation="relu"),
+                keras.layers.Dense(10, activation="softmax"),
+            ],
         )
-
-        try:
-            self.model.load_weights(self.checkpoint_filepath)
-        except tf.errors.NotFoundError:
-            pass
-
-        self.model.fit(
-            train_ds,
-            epochs=epochs,
-            validation_data=test_ds,
-            callbacks=[self.checkpoint_callback],
-        )
-        self.model.evaluate(test_ds)
 
 
 if __name__ == "__main__":
-    mnist = Mnist()
+    train_set, test_set = MnistDataset()
+    mnist = Mnist(train_set, test_set, (28, 28, 1))
     mnist.train()
