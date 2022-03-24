3725240999d924f0b8c275e475ff8ddc451b1039
beta <beta.underalpha@gmail.com>
2022-03-24 12:37:01 +0900

    (#4) Refactor: add dataset mixin

--- src/typings/dataset.py
+++ src/typings/dataset.py
@@ -1,10 +1,23 @@
 from abc import ABC, abstractmethod
-from typing import NewType
+from typing import NewType, Tuple, Protocol
 
+import numpy as np
 import tensorflow as tf
+
+keras = tf.keras
 
 TestSet = NewType("TestSet", tf.data.Dataset)
 TrainSet = NewType("TrainSet", tf.data.Dataset)
+
+NumpyDataset = Tuple[np.array, np.array]
+
+
+class DatasetPostprocessMixin(Protocol):
+    @staticmethod
+    def process(
+        train: NumpyDataset, test: NumpyDataset
+    ) -> Tuple[NumpyDataset, NumpyDataset]:
+        ...
 
 
 class Dataset(ABC):
@@ -13,5 +26,30 @@ class Dataset(ABC):
     def dataset() -> (TestSet, TrainSet):
         pass
 
-    def __new__(cls, *args, **kwargs):
-        return cls.dataset()
+
+
+class GrayscaleMixin(DatasetPostprocessMixin):
+    @staticmethod
+    def process(
+        train: NumpyDataset, test: NumpyDataset
+    ) -> Tuple[NumpyDataset, NumpyDataset]:
+        (x_train, y_train), (x_test, y_test) = train, test
+
+        x_train = x_train[..., tf.newaxis].astype("float32")
+        x_test = x_test[..., tf.newaxis].astype("float32")
+
+        return (x_train, y_train), (x_test, y_test)
+
+
+class NoisyMixin(DatasetPostprocessMixin):
+    @staticmethod
+    def process(
+        train: NumpyDataset, test: NumpyDataset
+    ) -> Tuple[NumpyDataset, NumpyDataset]:
+        def noisy(ds: np.array):
+            noise = 0.1 * np.random.normal(size=np.shape(ds))
+            return np.clip(ds + noise, 0.0, 1.0)
+
+        (x_train, y_train), (x_test, y_test) = train, test
+
+        return (noisy(x_train), x_train), (noisy(x_test), x_test)
