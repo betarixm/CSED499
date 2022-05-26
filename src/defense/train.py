c780656c7a09e5e820a6bd0254434725c2fa43aa
beta <beta.underalpha@gmail.com>
2022-05-26 20:04:31 +0900

    (#3) Defense: Add logic for training targeted model

--- src/defense/train.py
+++ src/defense/train.py
@@ -1,4 +1,4 @@
-from utils.dataset import NoisyMnist, NoisyCifar10, ExCifar10
+from utils.dataset import NoisyMnist, NoisyCifar10, ExCifar10, TgCifar10
 from models import Reformer, Exformer
 
 import argparse
@@ -43,6 +43,18 @@ def train_excifar10_exformer(epochs: int = 100):
     exformer.train(epochs)
 
 
+def train_tgcifar10_tgformer(epochs: int = 100):
+    train_set, test_set = TgCifar10().dataset()
+    tgformer = Exformer(
+        "defense_tgformer_cifar10",
+        (32, 32, 3),
+        1.0,
+        train_set,
+        test_set,
+    )
+    tgformer.train(epochs)
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Training defensive reformer models.")
     parser.add_argument(
@@ -52,7 +64,7 @@ if __name__ == "__main__":
         type=str,
         help="Dataset for training",
         required=True,
-        choices=["mnist", "cifar10", "excifar10"],
+        choices=["mnist", "cifar10", "excifar10", "tgcifar10"],
     )
 
     parser.add_argument(
@@ -74,3 +86,5 @@ if __name__ == "__main__":
         train_cifar10_reformer(e)
     elif args.dataset == "excifar10":
         train_excifar10_exformer(e)
+    elif args.dataset == "tgcifar10":
+        train_tgcifar10_tgformer(e)
